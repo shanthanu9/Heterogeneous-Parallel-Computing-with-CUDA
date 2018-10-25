55a3c94fdd64cdd758c4aef9e1141d6c3313d209
shanthanu <shanthanu.s.rai9@gmail.com>
2018-10-25 21:55:45 +0530

    Added Q3 of A2

---
+++ A2/Tiled-Matrix-Multiplication/A2Q3.cu
@@ -0,0 +1,146 @@
+//Tiled Matrix Multiplication
+#include <stdio.h>
+#define BLOCK_SIZE 32
+#define CEIL(a, b) ((a-1)/b +1)
+#define TILE 32
+const int N = 800;
+
+__global__ void matrix_multiplicaton(int *d_out, int *d_in1, int *d_in2) {
+	__shared__ int in_tile1[TILE][TILE], in_tile2[TILE][TILE];
+
+	int cj = blockIdx.x * blockDim.x + threadIdx.x;
+	int ci = blockIdx.y * blockDim.y + threadIdx.y;
+
+	int c = 0;
+
+	for(int k = 0; k < N; k += BLOCK_SIZE) {
+
+		int i = ci;
+		int j = k + threadIdx.x;
+
+		if(i < N && j < N) {
+			in_tile1[threadIdx.y][threadIdx.x] = d_in1[i*N+j];
+		}
+		else {
+			in_tile1[threadIdx.y][threadIdx.x] = 0;
+		}
+
+		i = k + threadIdx.y;
+		j = cj;
+
+		if(i < N && j < N) {
+			in_tile2[threadIdx.y][threadIdx.x] = d_in2[i*N+j];
+		}
+		else {
+			in_tile2[threadIdx.y][threadIdx.x] = 0;
+		}
+
+		__syncthreads();
+
+		for(int l = 0; l < BLOCK_SIZE; l++) {
+			c += (in_tile1[threadIdx.y][l] * in_tile2[l][threadIdx.x]);
+		}
+		
+		__syncthreads();
+	}
+
+	if(ci < N && cj < N) {
+		d_out[ci*N+cj] = c;
+	}
+
+}
+
+bool test_solution(int *h_out, int *h_in1, int *h_in2) {
+	bool flag = true;
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			int sum = 0;
+			for(int k = 0; k < N; k++) {
+				sum += (h_in1[i*N+k] * h_in2[k*N+j]);
+			}
+			//printf("%d ", sum);
+			if(sum != h_out[i*N+j]) {
+				flag = false;
+				break;
+			}	
+		}
+		//printf("\n");
+	}
+	if(flag) return true;
+	return false;
+}
+
+int main() {
+	srand(time(0));
+
+	int h_in1[N * N], h_in2[N * N], h_out[N * N];
+
+	for(int i = 0; i < N * N; i++) {
+        h_in1[i] = rand()%1000;
+    }
+
+
+	for(int i = 0; i < N * N; i++) {
+		h_in2[i] = rand()%1000;
+	}
+
+	int *d_in1, *d_in2, *d_out;
+
+	cudaMalloc(&d_in1, N * N * sizeof(int));
+	cudaMalloc(&d_in2, N * N * sizeof(int));
+	cudaMalloc(&d_out, N * N * sizeof(int));
+
+	cudaMemcpy(d_in1, h_in1, N * N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_in2, h_in2, N * N * sizeof(int), cudaMemcpyHostToDevice);
+
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaEventRecord(start);
+	
+	dim3 grid(CEIL(N, BLOCK_SIZE), CEIL(N, BLOCK_SIZE), 1);
+	dim3 block(BLOCK_SIZE, BLOCK_SIZE, 1);
+
+	matrix_multiplicaton<<<grid, block>>>(d_out, d_in1, d_in2);
+
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	float milliseconds = 0;
+	cudaEventElapsedTime(&milliseconds, start, stop);
+
+	cudaMemcpy(h_out, d_out, N * N * sizeof(int), cudaMemcpyDeviceToHost);
+
+	/*
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			printf("%d ", h_out[i*N+j]);
+		}
+		printf("\n");
+	}
+	*/
+	clock_t cpu_startTime, cpu_endTime;
+	double cpu_ElapseTime=0;
+	cpu_startTime = clock();
+	
+	bool flag = test_solution(h_out, h_in1, h_in2);
+
+	cpu_endTime = clock();
+	cpu_ElapseTime = ((cpu_endTime - cpu_startTime)/(1.0 * CLOCKS_PER_SEC)) * 1000;
+
+	if(flag) {
+		printf("The computed matrix is correct!\n");
+		printf("Time taken by GPU : %f ms\n", milliseconds);
+		printf("Time taken by CPU : %f ms\n", cpu_ElapseTime);
+	}
+	else {
+		printf("The computed matrix is incorrect!\n");
+	}
+
+	cudaFree(d_in1);
+	cudaFree(d_in2);
+	cudaFree(d_out);
+
+	return 0;
+}
+
---
+++ A2/Tiled-Matrix-Multiplication/README.md
@@ -0,0 +1,9 @@
+# Tiled matrix multiplication routine using shared memory
+
+For two random matrices of the order 800X700 and 700X900, the obtained performance metrics were, 
+
+>Time taken by GPU : 2.870624 ms
+>Time taken by CPU : 1600.000000 ms
+
+Approximate speed up achieved : 557.5
+
